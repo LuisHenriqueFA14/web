4deea0777a50e4b3e50d95fcf0308b8767a94fec
Luis Henrique <luish.fa1515@gmail.com>
2021-08-03 15:26:25 -0300

    Error page :fire:

--- src/commands/cli/errors/no_content_error.cpp
+++ src/commands/cli/errors/no_content_error.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <string>
 
+#include "../../../../include/rang.hpp"
+
 using namespace std;
 
 void no_content_error(string platform) {
-	; // Error
+	cout << endl << "    " << rang::fg::red << "[ERROR] No search content" << rang::fg::reset << endl;
+
+	cout << endl;
+
+	cout << "    " << "Right usage:" << endl;
+	cout << "    " << "  " << rang::fg::yellow << "web " << platform << " <SEARCH CONTENT>" << rang::fg::reset << endl;
+
+	cout << endl << "    " << "To show " << rang::fg::gray << "help" << rang::fg::reset << " page, type " << rang::fg::magenta << "web" << rang::fg::reset << ". 😜" << endl << endl;
 }
